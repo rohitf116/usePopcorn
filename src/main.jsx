3df09575aaa110d11e926ca81ff738fedae82435
rohitf116 <rrsonawne090909@gmail.com>
2023-07-03 13:05:47 -0700

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+// import "./index.css";
+import StarRating from "./StarRating.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App />
+     */}
+    <StarRating maxRating={7} />
+    <StarRating />
   </React.StrictMode>
 );
